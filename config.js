9013e5cd36cedd953e1325006c0e0e76a35cf80c
jxu <7989982+jxu@users.noreply.github.com>
2020-05-30 03:29:45 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,108 @@
+var CONFIG = {
+  "maps": {
+    "rdf_iso": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590786179,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "RDF isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "rdf_isometric_t4",
+      "tileSize": 512,
+      "world": "rdf",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "rdf_top": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590786179,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "RDF top-down",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "rdf_topdown_t4",
+      "tileSize": 256,
+      "world": "rdf",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "rdf_top",
+    "rdf_iso"
+  ],
+  "tileSetGroups": {
+    "rdf_isometric_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    },
+    "rdf_topdown_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    }
+  }
+};
